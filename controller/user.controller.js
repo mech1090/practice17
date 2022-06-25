b62e425bfcaa71a9589368a22dd01d44909ff1bd
Shahzad <mech1090@gmail.com>
2022-06-24 18:41:41 -0600

    route and controller  created

---
+++ controller/user.controller.js
@@ -0,0 +1,15 @@
+
+
+const getLoginForm = (req,res)=>{
+    return res.render('login/layout')
+}
+
+const login = async(req,res)={}
+
+const getSignupForm = (req,res)=>{
+    return res.render('signup/layout')
+}
+
+const signup = async(req,res)=>{
+    
+}
